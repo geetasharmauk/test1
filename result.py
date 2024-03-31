c2c566934c7184e0cd6842c773f27b45ce04d81e
geetasharmauk <165437179+geetasharmauk@users.noreply.github.com>
2024-03-31 09:56:09 +0100

    result

---
+++ result.py
@@ -0,0 +1,11 @@
+def result(percentage):
+    if percentage >=60:
+       print ("pass first division")
+    elif percentage >=40:
+       print ("pass second division")
+    else:
+       print ("fail")
+
+
+result (55)
+
