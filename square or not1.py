3729576f3cf01e08809b820ecc2daf4d5f8f08af
geetasharmauk <165437179+geetasharmauk@users.noreply.github.com>
2024-04-01 18:08:37 +0100

    Create square or not1.py

---
+++ square or not1.py
@@ -0,0 +1,14 @@
+print ("length:")
+length = input()
+print ("breadth:")
+breadth = input ()
+
+if length == breadth:
+   print ("square")
+else:
+   print ("rectangle")
+
+#assigning specific values
+length = 5
+breadth = 3
+
